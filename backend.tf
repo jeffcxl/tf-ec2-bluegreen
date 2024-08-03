7fbf35d7f1c5dcaf80eae8e9e699590f64e05214
jeffcxl <jeffcxl@hotmail.com>
2024-08-03 12:07:34 +0800

    fmt

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce6-tfstate"               # Terraform State bucket name
-    key    = "jeff-ec2-bluegreen.tfstate"  # Name of your tfstate file
-    region = "ap-southeast-1"                 # Terraform State bucket region
+    bucket = "sctp-ce6-tfstate"           # Terraform State bucket name
+    key    = "jeff-ec2-bluegreen.tfstate" # Name of your tfstate file
+    region = "ap-southeast-1"             # Terraform State bucket region
   }
 }
